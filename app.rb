e97847e4f8c7c0eb2a7ff53dd5d4cb6f5d52ebca
sandytan10 <stan10@stuy.edu>
2018-08-17 00:11:33 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,17 +2,21 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  # This is a sample static route.
-  get '/hello' do
-    "Hello World!"
-  end
-
+ 
   # This is a sample dynamic route.
   get "/hello/:name" do
     @user_name = params[:name]
+    #@user_name = instance variable you're setting it equal to the name
     "Hello #{@user_name}!"
   end
 
   # Code your final two routes here:
+
+
+get "/goodbye/:name" do 
+  @user_name = params[:name]
+  "goodbye #{@user_name}"
+  erb :goodbye 
+end
 
 end
